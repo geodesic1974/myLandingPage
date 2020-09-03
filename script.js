7d80ed2ca9bfce98989b7cb86db2323c24ad6bad
Patricia Donnell <70522446+geodesic1974@users.noreply.github.com>
2020-09-03 13:31:55 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function validateForm() {
+  var x = document.forms["form"]["email"].value;
+  if (x == "") {
+    alert("Email must be filled out");
+    return false;
+  }
+} 
+
+function ValidateEmail(inputText)
+{
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if(inputText.value.match(mailformat))
+{
+document.form.email.focus();
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+document.form.email.focus();
+return false;
+}
+}
